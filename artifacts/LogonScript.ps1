48ffc3be7acd40fa66f996a7d7db8014ca542c36
Jagadish Murugan <jagamu@microsoft.com>
2026-04-15 16:46:40 -0700

    Comment out UAMI ID login and web request
    Comment out the login and web request commands for the UAMI ID.

--- artifacts/LogonScript.ps1
+++ artifacts/LogonScript.ps1
@@ -23,9 +23,9 @@ Start-Transcript -Path C:\Temp\LogonScript.log
 
 Write-Host "Starting the script execution..."
 
-$uamiId = "0edf3d3b-2f30-453e-8e40-52c488f48961"
-az login --identity --username $uamiId
-Invoke-WebRequest -Uri $msiUrl -OutFile "C:\Temp\aio-k3s.msi"
+#$uamiId = "0edf3d3b-2f30-453e-8e40-52c488f48961"
+#az login --identity --username $uamiId
+#Invoke-WebRequest -Uri $msiUrl -OutFile "C:\Temp\aio-k3s.msi"
 
 # The federated token is short lived so convert it immediately to tokens with longer lifetime.
 # Convert federated token to ARM access token
